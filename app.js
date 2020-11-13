e11af089c4f74e4ef46614de7647efacf851b94e
miniozaur <ancfal.michal@gmail.com>
2020-11-13 13:36:41 +0100

    pierwszy commit do remotea z kursu node.js

---
+++ app.js
@@ -0,0 +1,18 @@
+function sayHello(name){
+    console.log('hello '+ name);
+}
+
+sayHello('Minio i Ola'); /*console.log(); - console is a global object 
+                            meaning it is a part of global scope
+                            so it can be accessed from anywhere */ 
+setTimeout(); 
+clearTimeout()
+setInterval();
+clearInterval();
+
+                            /*more global methods, 
+                            for setting timeout and time intervals 
+                            for calling methods after a time or repeatedly  */
+
+//global object in node is similar to window in browsers 
+
