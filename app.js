571c19836329e742271cafdc7d5625bec917222d
miniozaur <ancfal.michal@gmail.com>
2020-11-16 08:15:37 +0100

    app.js creation and update

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ function sayHello(name){
 sayHello('Minio i Ola'); /*console.log(); - console is a global object 
                             meaning it is a part of global scope
                             so it can be accessed from anywhere */ 
-setTimeout(); 
+setTimeout(11); 
 clearTimeout()
-setInterval();
+setInterval(111);
 clearInterval();
 
                             /*more global methods, 
@@ -15,4 +15,4 @@ clearInterval();
                             for calling methods after a time or repeatedly  */
 
 //global object in node is similar to window in browsers 
-
+ 
