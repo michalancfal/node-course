1600ebd1133de846c43f5e9c97f3476cbecd52a4
miniozaur <ancfal.michal@gmail.com>
2020-11-18 07:24:55 +0100

    lesson1 freecodecamp

---
+++ lesson1.js
@@ -0,0 +1,17 @@
+// inline comment HEROKU PASS Manam135% 
+
+/* multi
+line
+comment */
+
+// data types 
+
+/* undefined - something that has not been defined 
+null - nothing but not zero 
+boolean - true or false 
+string - string of characters
+symbol - unique primitive value, used for unique identifiers 
+object - special non-data but structural type for any constructed object instance 
+also used as data structures: new Object, new Array, new Map, new Set, new WeakMap, new WeakSet,
+new Date and almost everything made with new keyword; 
+*/
